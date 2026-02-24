40ff5aa0da707b6261a940fcdb84ff1d2fca0391
Sergey Agaka <sergeyagaka@gmail.com>
2026-02-24 14:47:42 +0300

    Работает, но не не нравится сложность алгоритма

--- src/biginteger_literals.cpp
+++ src/biginteger_literals.cpp
@@ -1,13 +1,11 @@
 #include "biginteger_literals.h"
+#include <string>
 
-namespace bi_literals {
-
-BigInteger operator""_bi(unsigned long long value) {
-    return BigInteger(static_cast<uint64_t>(value));
+// Реализация
+BigInteger operator"" _bi(unsigned long long value) {
+    return BigInteger(uint64_t(value));
 }
 
-BigInteger operator""_bi(const char* str, std::size_t len) {
+BigInteger operator"" _bi(const char* str, std::size_t len) {
     return BigInteger(std::string(str, len));
-}
-
 }
--- src/biginteger_literals.h
+++ src/biginteger_literals.h
@@ -1,11 +1,6 @@
 #pragma once
-
-#include <cstddef>
 #include "biginteger.h"
 
-namespace bi_literals {
-
-    BigInteger operator""_bi(unsigned long long value);
-    BigInteger operator""_bi(const char* str, std::size_t);
-
-}
+// Объявления
+BigInteger operator"" _bi(unsigned long long);
+BigInteger operator"" _bi(const char*, std::size_t);
